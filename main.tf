6567223472f758f887d4cc08ec7b5727016b4d0a
Ukang'a Dickson <ukanga@users.noreply.github.com>
2020-09-26 21:26:24 +0300

    Set authentication variables
    The variables by default are optional but if present will be set
    accordingly.
    
    Closes https://github.com/onaio/terraform-module-uptimerobot/issues/4

--- main.tf
+++ main.tf
@@ -18,12 +18,15 @@ data "uptimerobot_alert_contact" "alert_contact" {
 resource "uptimerobot_monitor" "monitor" {
   for_each = var.uptimerobot_monitors
 
-  friendly_name = each.value.friendly_name
-  type          = each.value.monitor_type
-  keyword_type  = each.value.keyword_type
-  keyword_value = each.value.keyword_value
-  url           = each.value.url
-  interval      = each.value.interval
+  friendly_name  = each.value.friendly_name
+  type           = each.value.monitor_type
+  keyword_type   = each.value.keyword_type
+  keyword_value  = each.value.keyword_value
+  url            = each.value.url
+  interval       = each.value.interval
+  http_username  = lookup(each.value, "http_username", null)
+  http_password  = lookup(each.value, "http_password", null)
+  http_auth_type = lookup(each.value, "http_auth_type", null)
 
   dynamic "alert_contact" {
     for_each = toset(each.value.alert_contacts)
